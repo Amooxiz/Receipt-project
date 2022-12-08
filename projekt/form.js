3fd5c786e4b12ca653e5226ac8167ec2bc63955b
Fipek <filip.bialowas2504@gmail.com>
2022-12-08 21:54:11 +0100

    finished

--- projekt/form.js
+++ projekt/form.js
@@ -1,9 +1,149 @@
+const form = document.querySelector("#productForm");
+const table = document.getElementsByTagName("table")[0];
+const editButton = document.getElementById("edit");
+const submitButton = document.getElementById("submit");
+const deleteButton = document.getElementById("delete");
+const sumDiv = document.getElementById("summed")
+let selectedRow = null;
+let selectedIndex = null;
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+
+
+
+
+
 function hasTextValue(input) {
   if (input.value.trim() === "") {
     return false;
   }
   return true;
 }
+
+const selectProduct = (e) => {
+  if(selectedRow!==null){
+
+    selectedRow.style.backgroundColor = "rgb(85, 26, 163)"
+  }
+  selectedRow = e.currentTarget;
+  selectedRow.style.backgroundColor = 'black';
+  selectedIndex = +selectedRow.children[0].innerText - 1;
+  form.elements["name"].value = selectedRow.children[1].innerText;
+  form.elements["quantity"].value = selectedRow.children[2].innerText;
+  form.elements["price"].value = selectedRow.children[3].innerText;
+  editButton.disabled = false;
+  deleteButton.disabled = false;
+};
+const sumUp = () =>{
+  const sum = products.reduce((acc, obj)=> { return acc + obj.sum; }, 0) || 0;
+  sumDiv.innerText = `SUMA: ${sum}`;
+  if(products.length ===0){
+    sumDiv.innerText = `SUMA: ${0}`;
+  }
+}
+const fixProductsId = () => {
+  products.forEach((product, index) => {
+    product.id = index;
+  });
+  products.sort((a,b) =>a.id-b.id);
+  const idCells = document.querySelectorAll("#id");
+  idCells.forEach((element, index) => {
+    element.innerHTML = index + 1;
+  });
+};
+const deleteProduct = (event) => {
+  event.preventDefault();
+  selectedRow.remove();
+  products = products.filter((product) => product.id !== selectedIndex);
+
+  fixProductsId();
+  editButton.disabled = true;
+  deleteButton.disabled = true;
+  clearForm();
+  sumUp();
+  selectedIndex = null;
+  selectedRow = null;
+  localStorage.setItem("products", JSON.stringify(products));
+};
+const editProduct = (event) => {
+  event.preventDefault();
+  selectedRow.children[1].innerText = form.elements["name"].value;
+  selectedRow.children[2].innerText = form.elements["quantity"].value;
+  selectedRow.children[3].innerText = form.elements["price"].value;
+  selectedRow.children[4].innerText =
+    +form.elements["price"].value * +form.elements["quantity"].value;
+  products[selectedIndex] = {
+    id: selectedIndex,
+    name: form.elements["name"].value,
+    quantity: +form.elements["quantity"].value,
+    price: +form.elements["price"].value,
+    sum: +form.elements["quantity"].value * +form.elements["price"].value,
+  };
+  editButton.disabled = true;
+  deleteButton.disabled = true;
+  clearForm();
+  selectedRow.style.backgroundColor = "rgb(85, 26, 163)"
+  selectedRow = null;
+  selectedIndex = null;
+  sumUp();
+  localStorage.setItem("products", JSON.stringify(products));
+
+};
+
+const clearForm = () => {
+  form.elements["name"].value = "";
+  form.elements["quantity"].value = "";
+  form.elements["price"].value = "";
+};
+
+const liftProduct = () => {
+  if (selectedIndex !== 0) {
+    const cells = document.querySelectorAll('#cell');
+    const tempCell = cells.item(selectedIndex-1);
+    const currCell = cells.item(selectedIndex);
+    const copyTempCell = tempCell.cloneNode(true);
+    const copyCurrCell = currCell.cloneNode(true);
+    copyTempCell.addEventListener('click',selectProduct);
+    copyCurrCell.addEventListener('click',selectProduct);
+    copyCurrCell.childNodes[5].childNodes[0].addEventListener('click',liftProduct);
+    copyTempCell.childNodes[5].childNodes[0].addEventListener('click',liftProduct);
+    copyCurrCell.childNodes[5].childNodes[1].addEventListener('click',dropProduct);
+    copyTempCell.childNodes[5].childNodes[1].addEventListener('click',dropProduct);
+    currCell.replaceWith(copyTempCell);
+    tempCell.replaceWith(copyCurrCell);  
+    const tempProduct = products[selectedIndex];
+    products[selectedIndex] = products[selectedIndex-1];
+    products[selectedIndex-1] = tempProduct;
+    fixProductsId();
+    clearForm();
+    copyCurrCell.style.backgroundColor =  "rgb(85, 26, 163)"
+  }
+};
+const dropProduct = () =>{
+  if(selectedIndex !== products.length-1){
+    const cells = document.querySelectorAll('#cell');
+    const tempCell = cells.item(selectedIndex+1);
+    const currCell = cells.item(selectedIndex);
+    const copyTempCell = tempCell.cloneNode(true);
+    const copyCurrCell = currCell.cloneNode(true);
+    copyTempCell.addEventListener('click',selectProduct);
+    copyCurrCell.addEventListener('click',selectProduct);
+    copyCurrCell.childNodes[5].childNodes[0].addEventListener('click',liftProduct);
+    copyCurrCell.childNodes[5].childNodes[1].addEventListener('click',dropProduct);
+    copyTempCell.childNodes[5].childNodes[0].addEventListener('click',liftProduct);
+    copyTempCell.childNodes[5].childNodes[1].addEventListener('click',dropProduct);
+    currCell.replaceWith(copyTempCell);
+    tempCell.replaceWith(copyCurrCell);  
+    const tempProduct = products[selectedIndex];
+    products[selectedIndex] = products[selectedIndex+1];
+    products[selectedIndex+1] = tempProduct;
+    fixProductsId();
+    copyCurrCell.style.backgroundColor =  "rgb(85, 26, 163)"
+  }
+}
+
+editButton.addEventListener("click", editProduct);
+deleteButton.addEventListener("click", deleteProduct);
 
 function validateNumber(input) {
   if (input.value <= 0) {
@@ -48,61 +188,30 @@ function clearBox(id) {
   document.getElementById(id).innerHTML = "";
 }
 
-// function createTable(array, product) {
-//     clearBox("PForm");
-//     let div = document.getElementById("PForm");
-
-//     const tbl = document.createElement("table");
-//     const tblBody = document.createElement("tbody");
-
-//     for (let i = -1; i < array.length; i++) {
-//         const row = document.createElement("tr");
-
-//         if (i === -1)
-//         {
-//             for (var prop in product) {
-//                 if (Object.prototype.hasOwnProperty.call(product, prop)) {
-//                     const cell = document.createElement("td");
-//                     const cellTxt = document.createTextNode(prop);
-
-//                     cell.appendChild(cellTxt);
-//                     row.appendChild(cell);
-//                 }
-//             }
-//             tblBody.appendChild(row);
-//             continue;
-//         }
-
-//         for (const [key, value] of Object.entries(array[i])) {
-//             const cell = document.createElement("td");
-//             const cellTxt = document.createTextNode(value);
-
-//             cell.appendChild(cellTxt);
-//             row.appendChild(cell);
-
-//         }
-//         tblBody.appendChild(row);
-//     }
-
-//     tbl.appendChild(tblBody);
-
-//     //document.body.appendChild(tbl);
-//     div.appendChild(tbl);
-//     tbl.setAttribute("border", "2");
-// }
-
-const createTd = (value) => {
+const createTd = (value, field) => {
   const cell = document.createElement("td");
   cell.innerText = value;
+  cell.id = field;
   return cell;
 };
 
-const form = document.querySelector("#productForm");
-const table = document.getElementsByTagName("table")[0];
-const products = [];
-localStorage.setItem("products", JSON.stringify(products));
+const createTdWithButtons = () => {
+  const cell = document.createElement("td");
+  const upButton = document.createElement("button");
+  const downButton = document.createElement("button");
+  upButton.innerText = "UP";
+  downButton.innerText = "DOWN";
+  upButton.id = "up";
+  downButton.id = "down";
+  downButton.id = "true";
+  cell.append(upButton);
+  cell.append(downButton);
+  upButton.addEventListener("click", liftProduct);
+  downButton.addEventListener("click", dropProduct);
+  return cell;
+};
 
-form.addEventListener("submit", function (event) {
+submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   if (
     validateFields(
@@ -112,23 +221,40 @@ form.addEventListener("submit", function (event) {
     )
   ) {
     const product = {
-      id: Math.random(),
+      id: products.length,
       name: form.elements["name"].value,
       quantity: form.elements["quantity"].value,
       price: form.elements["price"].value,
       sum: form.elements["quantity"].value * form.elements["price"].value,
     };
-    // const prodcutsFormLS = localStorage.getItem("products") || [];
-    // const arr = JSON.parse(prodcutsFormLS);
     products.push(product);
     const tr = document.createElement("tr");
+    tr.id = "cell";
+    tr.addEventListener("click", selectProduct);
+
+    tr.append(createTd(products.length, "id"));
+    tr.append(createTd(product.name, "name"));
+    tr.append(createTd(product.quantity, "quantity"));
+    tr.append(createTd(product.price, "price"));
+    tr.append(createTd(product.sum, "sum"));
+    tr.append(createTdWithButtons());
     table.append(tr);
-    table.append(createTd(products.length));
-    table.append(createTd(product.name));
-    table.append(createTd(product.quantity));
-    table.append(createTd(product.price));
-    table.append(createTd(product.sum));
     localStorage.setItem("products", JSON.stringify(products));
+    clearForm();
     // localStorage.products = JSON.stringify(arr);
+    sumUp();
   }
 });
+products.forEach(product =>{
+  const tr = document.createElement("tr");
+  tr.id = "cell";
+  tr.addEventListener("click", selectProduct);
+
+  tr.append(createTd(product.id, "id"));
+  tr.append(createTd(product.name, "name"));
+  tr.append(createTd(product.quantity, "quantity"));
+  tr.append(createTd(product.price, "price"));
+  tr.append(createTd(product.sum, "sum"));
+  tr.append(createTdWithButtons());
+  table.append(tr);
+})
